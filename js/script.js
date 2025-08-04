694195e801d5f500327b2e4378fd5e3165f6f63a
iam-rahees <raheesrahees32205@gmail.com>
2025-08-05 00:36:48 +0530

    toggle

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 $(window).scroll(function(){
     var x = $(this).scrollTop();
 
-    if( x >= 500){
+    if( x >= 50){
         $("header").addClass("header-bg");
     }
     else{
         $("header").removeClass("header-bg");
     }
+})
+$(document).ready(function(){
+    $('.toggle-bt').click(function(){
+    $('.toggle-bt').toggleClass('clicked');
+
+    });
 })
